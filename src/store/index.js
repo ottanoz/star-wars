87f1b5effa7180556a4be47d58811a708f0af7f5
Fernando Zonatto <>
2022-03-30 00:54:45 -0300

    Preventing warning on console

--- src/store/index.js
+++ src/store/index.js
@@ -10,7 +10,10 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    characters: [],
+    characters: {
+      count: 0,
+      results: [],
+    },
     characterDetails: {},
     loadingCharacters: false,
   },
